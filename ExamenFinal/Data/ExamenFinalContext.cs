1c9cb208ae4a26106da4374181c1d4fbec36eec1
camacho-fran <franccesca_3001@hotmail.com>
2026-01-08 20:12:27 -0500

    Agregar archivos de proyecto.

---
+++ ExamenFinal/Data/ExamenFinalContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExamenFinal.Models;
+
+namespace ExamenFinal.Data
+{
+    public class ExamenFinalContext : DbContext
+    {
+        public ExamenFinalContext(DbContextOptions<ExamenFinalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pago> Pagos { get; set; }
+    }
+}
---
+++ ExamenFinal/Models/Pago.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamenFinal.Models
+{
+    public class Pago
+    {
+        public int PagoId { get; set; }
+        public string MetodoPago { get; set; }
+        public DateTime FechaPago { get; set; }
+        public decimal Monto { get; set; }
+    }
+}
